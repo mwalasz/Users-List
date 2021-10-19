cc4336dc5972946aca3dfa46d8c90f50f9f95740
mwalasz <mwalasz@interia.pl>
2021-10-19 21:01:56 +0200

    Initialized project with Create React App

---
+++ src/views/Root/Root.js
@@ -0,0 +1,12 @@
+import { React } from 'react';
+import './Root.css';
+
+const Root = () => {
+    return (
+        <div className="container">
+            <p>Users list</p>
+        </div>
+    );
+};
+
+export default Root;
