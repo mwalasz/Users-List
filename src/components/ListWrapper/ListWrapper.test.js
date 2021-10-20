9a29abfa9f13d9cb22c2139a40cc08842f703223
mwalasz <mwalasz@interia.pl>
2021-10-20 21:49:50 +0200

    Added ListWrapper tests

---
+++ src/components/ListWrapper/ListWrapper.test.js
@@ -0,0 +1,80 @@
+import { render, screen, cleanup } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import { EMPTY_LIST } from '../../utils/texts';
+import ListWrapper from './ListWrapper';
+
+const users = [
+    {
+        id: 0,
+        name: 'Jan Kowalski',
+        username: 'jkowalski',
+    },
+    {
+        id: 1,
+        name: 'Adam Nowak',
+        username: 'anowak',
+    },
+];
+
+beforeEach(() => {
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+        json: jest.fn().mockResolvedValue(users),
+    });
+});
+
+afterEach(() => {
+    jest.restoreAllMocks();
+});
+
+afterAll(cleanup);
+
+describe('Tests of list filtration', () => {
+    test('Find all users', async () => {
+        render(<ListWrapper searchText={''} />);
+
+        const listContent = await screen.findAllByTestId('item');
+        expect(listContent).toHaveLength(2);
+        expect(screen.getByText('Jan Kowalski')).toBeInTheDocument();
+        expect(screen.getByText('Adam Nowak')).toBeInTheDocument();
+    });
+
+    test('Find one user', async () => {
+        render(<ListWrapper searchText={'adam'} />);
+
+        const listContent = await screen.findAllByTestId('item');
+        expect(listContent).toHaveLength(1);
+        expect(screen.getByText('Adam Nowak')).toBeInTheDocument();
+    });
+
+    test('Find one user with not matching mixed case', async () => {
+        render(<ListWrapper searchText={' KoWaLs'} />);
+
+        const listContent = await screen.findAllByTestId('item');
+        expect(listContent).toHaveLength(1);
+        expect(screen.getByText('Jan Kowalski')).toBeInTheDocument();
+    });
+
+    test('No user found', async () => {
+        await act(async () => {
+            render(<ListWrapper searchText={'abcdef'} />);
+        });
+
+        const listContent = screen.queryAllByTestId('item');
+        expect(listContent).toHaveLength(0);
+    });
+
+    test('Connection error', async () => {
+        const fetchError = jest
+            .spyOn(global, 'fetch')
+            .mockRejectedValueOnce('Timeout');
+        const windowAlert = jest
+            .spyOn(window, 'alert')
+            .mockImplementationOnce(jest.fn());
+
+        render(<ListWrapper searchText={''} />);
+
+        expect(await screen.findByText(EMPTY_LIST)).toBeInTheDocument();
+        expect(fetchError).toBeCalledTimes(1);
+        expect(windowAlert).toBeCalledTimes(1);
+    });
+});
