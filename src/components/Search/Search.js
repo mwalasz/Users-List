9473d102f8dd9eab47722454280aeaae60eabcd0
mwalasz <mwalasz@interia.pl>
2021-10-21 09:23:57 +0200

    Small refactor of Search

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,18 +2,16 @@ import { React } from 'react';
 import PropTypes from 'prop-types';
 import { SEARCH_PLACEHOLDER } from '../../utils/texts';
 
-const Search = ({ text, onTextChange }) => {
-    return (
-        <input
-            data-testid="search"
-            className="search"
-            type="search"
-            placeholder={SEARCH_PLACEHOLDER}
-            value={text}
-            onChange={onTextChange}
-        />
-    );
-};
+const Search = ({ text, onTextChange }) => (
+    <input
+        data-testid="search"
+        className="search"
+        type="search"
+        placeholder={SEARCH_PLACEHOLDER}
+        value={text}
+        onChange={onTextChange}
+    />
+);
 
 Search.propTypes = {
     text: PropTypes.string,
